115631c0400908629208099e3314cd881e98e97d
zaki <zaki.zahri@canang.com.my>
2018-04-17 12:34:31 +0800

    chapter3: Send data from the front end to the view model

--- Groceries/platforms/ios/Groceries/app/views/login/login.js
+++ Groceries/platforms/ios/Groceries/app/views/login/login.js
@@ -1,10 +1,14 @@
+var page;
+var email;
 var frameModule = require("ui/frame");
 
-exports.loaded = function() {
-    console.log("hello");
+exports.loaded = function(args) {
+    page = args.object;
 };
+
 exports.signIn = function() {
-    alert("Signing in");
+    email = page.getViewById("email");
+    console.log(email.text);
 };
 
 exports.register = function() {
