4b7965c426f5bee30331023d850ac1b35d925caf
zaki <zaki.zahri@canang.com.my>
2018-04-17 16:39:20 +0800

    chapter6: Edit the ListView

--- Groceries/app/views/list/list.js
+++ Groceries/app/views/list/list.js
@@ -1,3 +1,4 @@
+var swipeDelete = require("../../shared/utils/ios-swipe-delete");
 var socialShare = require("nativescript-social-share");
 var GroceryListViewModel = require("../../shared/view-models/grocery-list-view-model");
 var groceryList = new GroceryListViewModel([]);var pageData = new observableModule.fromObject({
@@ -19,6 +20,12 @@ var page;
 
 exports.loaded = function(args) {
     page = args.object;
+    if (page.ios) {
+        var listView = page.getViewById("groceryList");
+        swipeDelete.enable(listView, function(index) {
+            groceryList.delete(index);
+        });
+    }
     var listView = page.getViewById("groceryList");
     page.bindingContext = pageData;
 
